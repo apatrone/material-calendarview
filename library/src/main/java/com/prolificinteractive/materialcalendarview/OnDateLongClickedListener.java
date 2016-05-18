3e46a4303dc57f21618089d7d987f789e0d2becf
Agnès PATRONE <agnespatrone@hotmail.com>
2016-05-18 10:53:30 +0200

    added long click on days

---
+++ library/src/main/java/com/prolificinteractive/materialcalendarview/OnDateLongClickedListener.java
@@ -0,0 +1,7 @@
+package com.prolificinteractive.materialcalendarview;
+
+/**
+ * Created by apatrone on 18/05/16.
+ */
+public interface OnDateLongClickedListener {
+}
---
+++ library/src/main/java/com/prolificinteractive/materialcalendarview/spans/SquareSpan.java
@@ -0,0 +1,7 @@
+package com.prolificinteractive.materialcalendarview.spans;
+
+/**
+ * Created by apatrone on 13/05/16.
+ */
+public class SquareSpan {
+}
